e33ae52917a7c1e7dbea8d793fe11b4f2e589780
AbhayPratap9999 <abhaypratapsins99@gmail.com>
2026-09-24 17:18:25 +0530

    main files for the project

---
+++ frontend/src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+
+import App from "../src/App"
+import { AuthProvider } from "./context/AuthContext"
+
+import "../src/index.css"
+
+
+createRoot(
+  document.getElementById("root")
+).render(
+
+  <StrictMode>
+
+    <AuthProvider>
+
+      <App />
+
+    </AuthProvider>
+
+  </StrictMode>
+
+)
